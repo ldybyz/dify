d9c003fb42144380386ad923af31a0c0062b7c2d
robin <ldybyz@163.com>
2025-06-30 17:59:24 +0800

    add auth
    # Conflicts:
    #	docker/docker-compose.yaml
    #	web/service/share.ts

--- web/service/share.ts
+++ web/service/share.ts
@@ -289,10 +289,21 @@ export const textToAudioStream = (url: string, isPublicAPI: boolean, header: { c
 export const fetchAccessToken = async ({ appCode, userId, webAppAccessToken }: { appCode: string, userId?: string, webAppAccessToken?: string | null }) => {
   const headers = new Headers()
   headers.append('X-App-Code', appCode)
+
+  // 自定义代码
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString)
+  const tokenFromParams = urlParams.get('token') || ''; 
+  globalThis.localStorage.setItem('mtk',tokenFromParams)
+  headers.append('token', tokenFromParams)
+  // 自定义代码
+
   const params = new URLSearchParams()
   webAppAccessToken && params.append('web_app_access_token', webAppAccessToken)
   userId && params.append('user_id', userId)
   const url = `/passport?${params.toString()}`
+
+  // const url = userId ? `/passport?user_id=${encodeURIComponent(userId)}` : '/passport'
   return get(url, { headers }) as Promise<{ access_token: string }>
 }
 
